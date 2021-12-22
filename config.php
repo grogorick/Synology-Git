dfeea902678a7b4590b2dab51b65985b5c89bf67
Steve Grogorick <steve@grogorick.de>
2021-12-22 16:24:15 +0100

    move config data to separate file

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+define("CONFIG_DSM_SERVER", "#####.synology.me:5001");
+
+define("CONFIG_SERVER", "git.yournicedyndnsdomain.com");
+define("CONFIG_SSH_USER", "gituser");
+define("CONFIG_SSH_PORT", 22);
+define("CONFIG_GIT_BASE_PATH", "/volume1/git/");
+
+define("CONFIG_SESSION_TIMEOUT", 1800);
+define("CONFIG_NUM_LATEST_COMMITS", 5);
+
+?>
--- index.php
+++ index.php
@@ -9,17 +9,7 @@ Required Apps:
 
 
 
-define("CONFIG_DSM_SERVER", "#####.synology.me:5001");
-
-define("CONFIG_SERVER", "git.yournicedyndnsdomain.com");
-define("CONFIG_SSH_USER", "gituser");
-define("CONFIG_SSH_PORT", 22);
-define("CONFIG_GIT_BASE_PATH", "/volume1/git/");
-
-define("CONFIG_SESSION_TIMEOUT", 1800);
-define("CONFIG_NUM_LATEST_COMMITS", 5);
-
-
+require("config.php");
 
 define("TEXT_LOADING", "<i>( lädt ... )</i>");
 
